2deb1fdd07d22c2195a6ca8883e06f23377ed42b
Connor Worley <connorbworley@gmail.com>
2017-05-22 09:22:55 -0700

    Initial commit

---
+++ src/main/kotlin/com/team5499/robots/frc2017/Camera.kt
@@ -0,0 +1,7 @@
+package com.team5499.robots.frc2017
+
+import edu.wpi.first.wpilibj.CameraServer
+
+fun useCameraServer() {
+  CameraServer.getInstance().startAutomaticCapture()
+}
---
+++ src/main/kotlin/com/team5499/robots/frc2017/Robot.kt
@@ -0,0 +1,10 @@
+@file:JvmName("Robot")
+package com.team5499.robots.frc2017
+
+import edu.wpi.first.wpilibj.IterativeRobot
+
+class Robot() : IterativeRobot() {
+  override fun robotInit() {
+    println("RobotInit!")
+  }
+}
